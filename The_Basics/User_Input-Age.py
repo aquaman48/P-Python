ba3164f96b88109ae9ff4db6b5cc1f441fb9ca5e
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-02-25 19:04:38 -0800

    Added to program

--- The_Basics/User_Input-Age.py
+++ The_Basics/User_Input-Age.py
@@ -6,3 +6,16 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 #3. Displays users input of name and age. 
 print("\nHello,",name,". \nToday you told me you are ", age,"years old")
+
+
+#more fun with user input
+
+first = int(input("Please enter the smaller integer: "))
+second = int(input("Please enter the larger integer: "))
+
+while first > 0:
+	remainder = second % first
+	second = first
+	first = remainder
+
+print("The greatest common divisor is", second)
