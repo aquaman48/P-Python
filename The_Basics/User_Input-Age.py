c9f8e0437c039bd100630797226da1cfa476efff
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-02-25 18:58:44 -0800

    User input in Python

---
+++ The_Basics/User_Input-Age.py
@@ -0,0 +1,8 @@
+
+
+#1. Ask user to input name and store it as text string
+name = input("Enter your name: ")
+#2. Ask user to input age and store it as an int var.
+age = int(input("Enter your age: "))
+#3. Displays users input of name and age. 
+print("\nHello,",name,". \nToday you told me you are ", age,"years old")
