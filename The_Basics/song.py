46881781967cc6fb89b80e376eafd53a21513be4
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-03-04 19:04:48 -0800

    'song' function

---
+++ The_Basics/song.py
@@ -0,0 +1,13 @@
+
+
+def print_lyrics():
+	"""Insert lyrics"""
+	print("\nOh what do you do with a drunken sailor?\nOh what do you do with a drunken sailor?\nThrow em in the brig until the morning!!")
+
+def repeat_lyrics():
+	"""repeat the lyrics"""
+	print_lyrics()
+	print_lyrics()
+	print_lyrics()
+
+repeat_lyrics()
