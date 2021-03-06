c4953c0da611c19820e37f7f4bdfdc03b0dd5f3e
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-03-05 17:02:55 -0800

    Button Demo
    Uses breezypythongui.py file and is for simple button demo! Check it out!

---
+++ The_Basics/Libraries/Breezy-Python-GUI-Lib/button_demo.py
@@ -0,0 +1,45 @@
+from breezypythongui import EasyFrame
+
+class ButtonDemo(EasyFrame):
+	"""Illustrates command buttons and user events."""
+
+	def __init__(self):
+		"""Sets up the window, label, and buttons."""
+		EasyFrame.__init__(self)
+
+		#a single label in the first row
+		self.label = self.addLabel(text = "Hello there!!!!", 
+								   row = 0, column = 0, 
+								   columnspan = 2,
+								   sticky = "NSEW")
+
+		#Two command buttons in the second row
+		#handler methods supplied
+		
+		self.clearBtn = self.addButton(text = "Erase Greeting",
+									   row = 1, column = 0,
+									   command = self.clear)
+		self.restoreBtn = self.addButton(text = "Restore MFR", 
+										 row = 1, column = 1,
+										 state = "disabled", 
+										 command = self.restore)
+	#methods to handle user events
+	def clear(self):
+		"""Resets the label to empty string and updates the button states"""
+		self.label["text"] = ''
+		self.clearBtn["state"] = "disabled"
+		self.restoreBtn["state"] = "normal"
+
+	def restore(self):
+		"""Resets label to original text and updates button"""
+		self.label["text"] = "Magic MUTHA FUCKAHHHH!!"
+		self.clearBtn["state"] = "normal"
+		self.restoreBtn["state"] = "disabled"
+
+def main():
+    """Instantiates and pops up the window."""
+    ButtonDemo().mainloop()
+    
+if __name__ == "__main__":
+    main()
+    
